c9909b285334eefd538aedbcfc3d6603fba49a7d
SDeVuyst <silasdevuyst@hotmail.com>
2025-07-09 20:43:20 +0200

    removed auth token for testing

--- app/api.py
+++ app/api.py
@@ -6,9 +6,9 @@ router = APIRouter()
 
 @router.api_route("/gym.ics", methods=["GET", "HEAD"], response_class=Response)
 async def get_gym_feed(request: Request):
-    token = request.query_params.get("token")
-    if token != settings.secret_token:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+    # token = request.query_params.get("token")
+    # if token != settings.secret_token:
+    #     raise HTTPException(status_code=401, detail="Unauthorized")
 
     # For HEAD request, return headers only
     if request.method == "HEAD":
